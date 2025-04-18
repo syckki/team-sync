7113b0ab7ef3745b69efd7678bc55796da291e5c
jhonnymorales1 <40410041-jhonnymorales1@users.noreply.replit.com>
2025-04-18 15:30:21 +0000

    Restored to '23f4b7aa42f5b66c64306a5d7618b7ca424c127e'
    Replit-Restored-To: 23f4b7aa42f5b66c64306a5d7618b7ca424c127e

--- pages/_document.js
+++ pages/_document.js
@@ -38,10 +38,10 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
           
           {/* PWA Meta Tags */}
-          <meta name="application-name" content="SecureShare" />
+          <meta name="application-name" content="Encrypted Messaging App" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-          <meta name="apple-mobile-web-app-title" content="SecureShare" />
+          <meta name="apple-mobile-web-app-title" content="E2E Encrypt" />
           <meta name="description" content="End-to-end encrypted messaging with offline support" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
@@ -49,18 +49,7 @@ export default class MyDocument extends Document {
           
           {/* PWA Icons and Manifest */}
           <link rel="manifest" href="/manifest.json" />
-          <link rel="shortcut icon" href="/icons/icon-192x192.png" />
-          
-          {/* Apple PWA Icons */}
-          <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-192x192.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-192x192.png" />
-          <link rel="apple-touch-icon" sizes="167x167" href="/icons/icon-192x192.png" />
-          
-          {/* MS PWA support */}
-          <meta name="msapplication-config" content="none" />
-          <meta name="msapplication-TileColor" content="#3498db" />
-          <meta name="msapplication-tap-highlight" content="no" />
+          <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
